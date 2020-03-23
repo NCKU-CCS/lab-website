76e766687639df37c476698c6a19e93fbb6bd1f4
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2020-03-23 14:54:03 +0800

    refactor: complete navbar with RWD

--- src/components/navbar/Navlinks.jsx
+++ src/components/navbar/Navlinks.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import useWindowWidth from '../../hooks/useWindowWidth';
 
 const Container = styled.div`
+	display: flex;
+`;
+
+const ListContainer = styled.div`
 	width: 40vw;
 	display: flex;
 	justify-content: space-between;
@@ -17,9 +22,65 @@ const Container = styled.div`
 		text-decoration: none;
 		white-space: nowrap;
 	}
+
+	@media (max-width: 800px) {
+		position: absolute;
+		right: ${props => props.isOpen ? '0px' : '-150px'};
+		width: 150px;
+		display: inline-block;
+		transform: translateY(90px);
+		transition-duration: 0.3s;
+		li{
+			margin: 0.5rem 0px;
+			display: ${props => props.isOpen ? {} : 'none'}
+		}
+		a {
+			font-size: 1.25rem;
+		}
+	}
+`;
+
+const Burger = styled.div`
+	display: none;
+
+	span {
+		width: 40px;
+		height: 5px;
+		margin: 2px 0px;
+		background-color: #50B58E;
+		transition-duration: 0.3s;
+	}
+
+	:hover {
+		cursor: pointer;
+	}
+
+	@media (max-width: 800px) {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+
+		span:nth-child(1) {
+			transform: 
+				translateY(${props => props.isOpen ? '9px' : '0px'}) 
+				rotate(${props => props.isOpen ? '45deg' : '0deg'});
+		}
+
+		span:nth-child(2) {
+			opacity: ${props => props.isOpen ? 0 : 1};
+		}
+
+		span:nth-child(3) {
+			transform: 
+				translateY(${props => props.isOpen ? '-9px' : '0px'}) 
+				rotate(${props => props.isOpen ? '-45deg' : '0deg'});
+		}
+	}
 `;
 
 const Navlinks = () => {
+	const [burgerOpen, setBurgerOpen] = useState(false);
+	const width = useWindowWidth();
 	
 	const linkConfig = [
 		{name: 'Home', ref: '/'},
@@ -28,21 +89,34 @@ const Navlinks = () => {
 		{name: 'Members', ref: '/members'},
 		{name: 'Contact Us', ref: '/contact_us'},
 	];
+
+	useEffect(() => {
+		if(window.innerWidth > 800) {
+			setBurgerOpen(false);
+		}
+	}, [width]);
 	
 	return (
 		<Container>
-			{linkConfig.map((link, index) => (
-				<li>
-					<NavLink 
-						exact 
-						to = {link.ref} 
-						activeStyle = {{ color: '#50B58E', fontWeight: 'bold' }}
-						key = {index}
-					>
-						<p>{link.name}</p>
-					</NavLink>
-				</li>
-			))}
+			<ListContainer isOpen={burgerOpen}>
+				{linkConfig.map((link, index) => (
+					<li key={index}>
+						<NavLink 
+							exact 
+							to = {link.ref} 
+							activeStyle = {{ color: '#50B58E', fontWeight: 'bold' }}
+							key = {index}
+						>
+							<p>{link.name}</p>
+						</NavLink>
+					</li>
+				))}
+			</ListContainer>
+			<Burger isOpen={burgerOpen} onClick={() => {setBurgerOpen(!burgerOpen)}}>
+				<span />
+				<span />
+				<span />
+			</Burger>
 		</Container>
 	);
 }
--- src/theme/GlobalStyles.jsx
+++ src/theme/GlobalStyles.jsx
@@ -12,8 +12,9 @@ export default createGlobalStyle`
   }
 
   html, body {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
   }
 
   p {
